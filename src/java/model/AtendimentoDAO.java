150de0147248e99576cfb93d11a86a78bf17d34c
Jandy <Jandy@DESKTOP-VQ57RBI>
2021-08-04 18:16:08 -0300

    Correção

--- src/java/model/AtendimentoDAO.java
+++ src/java/model/AtendimentoDAO.java
@@ -19,7 +19,7 @@ public class AtendimentoDAO extends DataBase {
             a.setId(rs.getInt("id"));
             a.setPreco(rs.getDouble("preco"));
             a.setFormaPagamento(rs.getString("forma_pagamento"));
-            a.setStatus(rs.getString("status"));;
+            a.setStatus(rs.getString("status"));
             a.setFuncionario(fDAO.carregarPorId(rs.getInt("id_funcionario")));
             a.setCliente(cDAO.carregarPorId(rs.getInt("id_cliente")));
             a.setData(rs.getTimestamp("data"));
@@ -60,7 +60,7 @@ public class AtendimentoDAO extends DataBase {
     }
 
     public void inserirAtendimento(Atendimento a) throws Exception {
-        String sql = "INSET INTO Atendimento (data, hora, preco, forma_pagamento, data_pagamento, data_atendimento, registro_atendimento, cancelamento, id_funcionario, id_cliente) VALUES (?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO Atendimento (data, hora, preco, forma_pagamento, data_pagamento, data_atendimento, registro_atendimento, cancelamento, id_funcionario, id_cliente) VALUES (?,?,?,?,?,?,?,?,?,?)";
         this.conectar();
         PreparedStatement pstm = conn.prepareStatement(sql);
         pstm.setTimestamp(1, a.getData());
@@ -73,31 +73,12 @@ public class AtendimentoDAO extends DataBase {
         pstm.setTimestamp(8, a.getCancelamento());
         pstm.setInt(9, a.getFuncionario().getId());
         pstm.setInt(10, a.getCliente().getId());
-        pstm.execute();
-        this.desconectar();
-    }
-
-    public void alterarAtendimento(Atendimento a) throws Exception {
-        String sql = "UPDATE Atendimento SET data=?, hora=?, preco=?, forma_pagamento=?, data_pagamento=?, data_atendimento=?, registro_atendimento=?, cancelamento=?, id_funcionario=?, id_cliente=? WHERE id=?";
-        this.conectar();
-        PreparedStatement pstm = conn.prepareStatement(sql);
-        pstm.setTimestamp(1, a.getData());
-        pstm.setTimestamp(2, a.getHora());
-        pstm.setDouble(3, a.getPreco());
-        pstm.setString(4, a.getFormaPagamento());
-        pstm.setTimestamp(5, a.getDataPagamento());
-        pstm.setTimestamp(6, a.getDataAtendimento());
-        pstm.setTimestamp(7, a.getRegistroAtendimento());
-        pstm.setTimestamp(8, a.getCancelamento());
-        pstm.setInt(9, a.getFuncionario().getId());
-        pstm.setInt(10, a.getCliente().getId());
-        pstm.setInt(11, a.getId());
         pstm.execute();
         this.desconectar();
     }
 
     public void deletarAtendimento(int id) throws Exception {
-        String sql = "DELETE * FROM Atendimento WHERE id=?";
+        String sql = "DELETE FROM Atendimento WHERE id=?";
         this.conectar();
         PreparedStatement pstm = conn.prepareStatement(sql);
         pstm.setInt(1, id);
